65af779e9803a7a4774e378b9e8be58e037f439f
Oleksandr Solianyk <salym3k@gmail.com>
2022-10-02 23:34:58 +0300

    work slider

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const images = document.querySelectorAll('.slide__line img')
+const slide = document.querySelector('.slide__line')
+const slideBtn = document.querySelectorAll('.slide__btn-group input')
+
+let i = 0
+
+slide.addEventListener('click', () => {
+    slideBtn[i].classList.remove('active')
+    images[i].style.display = 'none'
+    i++
+    if (i >= images.length) i = 0
+    images[i].style.display = 'block'
+    slideBtn[i].classList.add('active')
+})
+
+slideBtn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        images.forEach(i => i.style.display = 'none')
+        slideBtn.forEach(i => i.classList.remove('active'))
+        i = index
+        images[i].style.display = 'block'
+        slideBtn[i].classList.add('active')
+    })
+})
